a728fc189a042f6e1c39b8eef19eb788c798364c
Juergen Adams <juergen@jekyll-one.com>
2020-09-30 18:58:35 +0200

    Replaced MMenu from version 2020.0.18

--- assets/themes/j1/adapter/js/navigator.js
+++ assets/themes/j1/adapter/js/navigator.js
@@ -18,7 +18,7 @@
  # NOTE: For getStyleValue helper see
  #  https://stackoverflow.com/questions/16965515/how-to-get-a-style-attribute-from-a-css-class-by-javascript-jquery
  # -----------------------------------------------------------------------------
- # Adapter generated: 2020-09-29 21:26:35 +0200
+ # Adapter generated: 2020-09-30 18:55:44 +0200
  # -----------------------------------------------------------------------------
 */
 // -----------------------------------------------------------------------------
@@ -79,7 +79,7 @@ j1.adapter['navigator'] = (function (j1, window) {
       // -----------------------------------------------------------------------
       var settings  = $.extend({
         module_name: 'j1.adapter.navigator',
-        generated:   '2020-09-29 21:26:35 +0200'
+        generated:   '2020-09-30 18:55:44 +0200'
       }, options);
       // -----------------------------------------------------------------------
       // options loader
