5519aea0e055e439147a5dbc928fea9351ba8088
Juergen Adams <juergen@jekyll.one>
2021-07-16 09:34:23 +0200

    New version 2021.1.12

--- assets/themes/j1/adapter/js/algolia.js
+++ assets/themes/j1/adapter/js/algolia.js
@@ -13,7 +13,7 @@
  # J1 Template is licensed under the MIT License.
  # For details, see https://jekyll.one
  # -----------------------------------------------------------------------------
- #  Adapter generated: 2021-07-16 07:18:35 +0000
+ #  Adapter generated: 2021-07-16 07:33:28 +0000
  # -----------------------------------------------------------------------------
 */
 // -----------------------------------------------------------------------------
@@ -51,7 +51,7 @@ j1.adapter['algolia'] = (function (j1, window) {
       // -----------------------------------------------------------------------
       var settings = $.extend({
         module_name: 'j1.adapter.algolia',
-        generated:   '2021-07-16 07:18:35 +0000'
+        generated:   '2021-07-16 07:33:28 +0000'
       }, options);
       _this.setState('started');
       logger.info('state: ' + _this.getState());
