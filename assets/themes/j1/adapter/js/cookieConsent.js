5240ef8b0aa8549edbeccbab7e979bf31560d106
Juergen Adams <juergen@jekyll.one>
2021-09-27 21:05:47 +0200

    Fixed robot settings to follow (_config.yml)

--- assets/themes/j1/adapter/js/cookieConsent.js
+++ assets/themes/j1/adapter/js/cookieConsent.js
@@ -16,7 +16,7 @@
  #  J1 Template is licensed under MIT License.
  #  See: https://github.com/jekyll-one/J1 Template/blob/master/LICENSE
  # -----------------------------------------------------------------------------
- #  Adapter generated: 2021-09-27 16:22:19 +0000
+ #  Adapter generated: 2021-09-27 19:00:50 +0000
  # -----------------------------------------------------------------------------
 */
 // -----------------------------------------------------------------------------
@@ -83,7 +83,7 @@ j1.adapter['cookieConsent'] = (function (j1, window) {
       // -----------------------------------------------------------------------
       var settings = $.extend({
         module_name: 'j1.adapter.cookieConsent',
-        generated:   '2021-09-27 16:22:19 +0000'
+        generated:   '2021-09-27 19:00:50 +0000'
       }, options);
       // Load  module DEFAULTS|CONFIG
       /* eslint-disable */
