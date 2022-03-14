c666e407eff2ff999c3a8c7ddd8fb219d0348292
Juergen Adams <juergen@jekyll.one>
2022-03-14 15:00:08 +0100

    New version 2022.1.3

--- assets/themes/j1/adapter/js/j1.js
+++ assets/themes/j1/adapter/js/j1.js
@@ -16,7 +16,7 @@
  #  TODO:
  #
  # -----------------------------------------------------------------------------
- # Adapter generated: 2022-03-11 16:27:00 +0000
+ # Adapter generated: 2022-03-14 13:56:47 +0000
  # -----------------------------------------------------------------------------
 */
 // -----------------------------------------------------------------------------
@@ -105,7 +105,7 @@ var j1 = (function () {
     'theme_name':           'UnoLight',
     'theme_css':            '',
     'theme_author':         'J1 Team',
-    'theme_version':        '2022.1.2',
+    'theme_version':        '2022.1.3',
     'session_active':       false,
     'google_translate':     'disabled',
     'translate_all_pages':  true,
@@ -140,7 +140,7 @@ var j1 = (function () {
       // -----------------------------------------------------------------------
       var settings = $.extend({
         module_name: 'j1',
-        generated:   '2022-03-11 16:27:00 +0000'
+        generated:   '2022-03-14 13:56:47 +0000'
       }, options);
       // -----------------------------------------------------------------------
       // Global variable settings
@@ -883,7 +883,7 @@ var j1 = (function () {
     // Returns the template version taken from site config (_config.yml)
     // -------------------------------------------------------------------------
     getTemplateVersion: function () {
-      return '2022.1.2';
+      return '2022.1.3';
     },
     // -------------------------------------------------------------------------
     // getScrollOffset()
--- assets/themes/j1/adapter/js/translator.js
+++ assets/themes/j1/adapter/js/translator.js
@@ -12,7 +12,7 @@
  #  J1 Template is licensed under MIT License.
  #  See: https://github.com/jekyll-one/J1 Template/blob/master/LICENSE
  # -----------------------------------------------------------------------------
- #  Adapter generated: 2022-03-11 16:27:00 +0000
+ #  Adapter generated: 2022-03-14 13:56:47 +0000
  # -----------------------------------------------------------------------------
 */
 // -----------------------------------------------------------------------------
@@ -64,7 +64,7 @@ j1.adapter.translator = (function (j1, window) {
       // -----------------------------------------------------------------------
       var settings = $.extend({
         module_name: 'j1.adapter.translator',
-        generated:   '2022-03-11 16:27:00 +0000'
+        generated:   '2022-03-14 13:56:47 +0000'
       }, options);
       // -----------------------------------------------------------------------
       // Global variable settings
