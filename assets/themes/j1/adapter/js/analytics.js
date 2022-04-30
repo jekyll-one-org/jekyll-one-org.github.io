634a99c3b9bc735ddab2cda45510cccebabcc53e
Juergen Adams <juergen@jekyll.one>
2022-04-30 21:50:59 +0200

    Disabled GA

--- assets/themes/j1/adapter/js/analytics.js
+++ assets/themes/j1/adapter/js/analytics.js
@@ -13,7 +13,7 @@
  # J1 Template is licensed under the MIT License.
  # For details, see: https://github.com/jekyll-one-org/j1-template/blob/main/LICENSE.md
  # -----------------------------------------------------------------------------
- #  Adapter generated: 2022-04-24 15:15:54 +0000
+ #  Adapter generated: 2022-04-30 19:48:45 +0000
  # -----------------------------------------------------------------------------
 */
 // -----------------------------------------------------------------------------
@@ -27,7 +27,7 @@ var url             = new liteURL(window.location.href);
 var hostname        = url.hostname;
 var environment     = 'development';
 var gaScript        = document.createElement('script');
-var providerID      = 'G-299QGW5RJH';
+var providerID      = '<your-tracking-id>';
 var validProviderID = (providerID.includes('your')) ? false : true;
 var optInOut        = true;
 var anonymizeIP     = true;
@@ -49,69 +49,13 @@ var logText;
     // adapter initializer
     // -------------------------------------------------------------------------
     init: function (options) {
-      // [INFO   ] [j1.adapter.analytics                    ] [ detected analytics provider (j1_config): google} ]
-      // [INFO   ] [j1.adapter.analytics                    ] [ start processing load region head, layout:  ]
-      // [INFO   ] [j1.adapter.analytics                    ] [ place provider: Google Adsense ]
-      // -----------------------------------------------------------------------
-      // Default module settings
-      // -----------------------------------------------------------------------
-      var settings = $.extend({
-        module_name: 'j1.adapter.analytics',
-        generated:   '2022-04-24 15:15:54 +0000'
-      }, options);
-      // -----------------------------------------------------------------------
-      // Global variable settings
-      // -----------------------------------------------------------------------
-      _this = j1.adapter.analytics;
-      logger = log4javascript.getLogger('j1.adapter.analytics.google');
-      // initialize state flag
-      _this.setState('started');
-      logger.debug('\n' + 'state: ' + _this.getState());
-      logger.info('\n' + 'module is being initialized');
-      // remove cookies on invalid GA config or left from a previous
-      // session/page view if they exists
-      // ---------------------------------------------------------------------
-      gaCookies = j1.findCookie('_ga');
-      gaCookies.forEach(function (item) {
-        logger.warn('\n' + 'delete cookie created by Google Analytics: ' + item);
-        if (hostname == 'localhost') {
-          j1.removeCookie({ name: item, domain: false, secure: false });
-        } else {
-          j1.removeCookie({ name: item, domain: '.' + hostname, secure: false });
-        }
-      });
       var dependencies_met_page_ready = setInterval(function() {
         if (j1.getState() == 'finished') {
-          gaExists = document.getElementById('google-tag-manager');
-          if (!gaExists) {
-            // add ga api dynamically in the head section
-            // -----------------------------------------------------------------
-            logger.info('\n' + 'Google Analytics API added in section: head');
-            gaScript.async = true;
-            gaScript.id    = 'google-tag-manager';
-            gaScript.src   = '//www.googletagmanager.com/gtag/js?id=' + providerID;
-            document.head.appendChild(gaScript);
-          }
-          user_consent  = j1.readCookie(cookie_names.user_consent);
-          if (user_consent.analysis) {
-            if (validProviderID) {
-              logger.info('\n' + 'user consent on analytics: ' + user_consent.analysis);
-              logger.info('\n' + 'enable Google Analytics on ID: ' + providerID);
-              GTagOptIn.register(providerID);
-              GTagOptIn.optIn();
-            } else {
-              logger.warn('\n' + 'invalid trackig id detected for Google Analytics: ' + providerID);
-            }
-          } else {
-            logger.info('\n' + 'user consent on analytics: ' + user_consent.analysis);
-            logger.warn('\n' + 'disable Google Analytics on ID: ' + providerID);
-            GTagOptIn.register(providerID);
-            GTagOptIn.optOut();
-          }
+          logger = log4javascript.getLogger('j1.adapter.analytics.google');
+          logger.info('\n' + 'Google Analytics: disabled');
           clearInterval(dependencies_met_page_ready);
-        } // END if getState 'finished'
+        }
       }, 25);
-      // [INFO   ] [j1.adapter.analytics                    ] [ end processing ]
       return;
     }, // END init
     // -------------------------------------------------------------------------
--- assets/themes/j1/adapter/js/translator.js
+++ assets/themes/j1/adapter/js/translator.js
@@ -12,7 +12,7 @@
  #  J1 Template is licensed under MIT License.
  #  See: https://github.com/jekyll-one/J1 Template/blob/master/LICENSE
  # -----------------------------------------------------------------------------
- #  Adapter generated: 2022-04-24 15:15:54 +0000
+ #  Adapter generated: 2022-04-30 19:48:45 +0000
  # -----------------------------------------------------------------------------
 */
 // -----------------------------------------------------------------------------
@@ -64,7 +64,7 @@ j1.adapter.translator = (function (j1, window) {
       // -----------------------------------------------------------------------
       var settings = $.extend({
         module_name: 'j1.adapter.translator',
-        generated:   '2022-04-24 15:15:54 +0000'
+        generated:   '2022-04-30 19:48:45 +0000'
       }, options);
       // -----------------------------------------------------------------------
       // Global variable settings
