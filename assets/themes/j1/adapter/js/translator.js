785ba463342c51861d122cb401dfc0f5e9c9c305
Juergen Adams <juergen@jekyll.one>
2022-05-14 15:07:16 +0200

    Latest files of version 2022.3.3

--- assets/themes/j1/adapter/js/translator.js
+++ assets/themes/j1/adapter/js/translator.js
@@ -12,7 +12,7 @@
  #  J1 Template is licensed under MIT License.
  #  See: https://github.com/jekyll-one/J1 Template/blob/master/LICENSE
  # -----------------------------------------------------------------------------
- #  Adapter generated: 2022-05-14 12:07:57 +0000
+ #  Adapter generated: 2022-05-14 12:52:38 +0000
  # -----------------------------------------------------------------------------
 */
 // -----------------------------------------------------------------------------
@@ -64,7 +64,7 @@ j1.adapter.translator = (function (j1, window) {
       // -----------------------------------------------------------------------
       var settings = $.extend({
         module_name: 'j1.adapter.translator',
-        generated:   '2022-05-14 12:07:57 +0000'
+        generated:   '2022-05-14 12:52:38 +0000'
       }, options);
       // -----------------------------------------------------------------------
       // Global variable settings
