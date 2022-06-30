08f40ac28d042134c1e38bcf621c3c31bddeb2e6
Juergen Adams <juergen@jekyll.one>
2022-06-30 15:14:31 +0200

    Removed example lanes from the home page

--- assets/themes/j1/adapter/js/translator.js
+++ assets/themes/j1/adapter/js/translator.js
@@ -12,7 +12,7 @@
  #  J1 Template is licensed under MIT License.
  #  See: https://github.com/jekyll-one/J1 Template/blob/master/LICENSE
  # -----------------------------------------------------------------------------
- #  Adapter generated: 2022-06-29 18:45:20 +0000
+ #  Adapter generated: 2022-06-30 13:11:19 +0000
  # -----------------------------------------------------------------------------
 */
 // -----------------------------------------------------------------------------
@@ -64,7 +64,7 @@ j1.adapter.translator = (function (j1, window) {
       // -----------------------------------------------------------------------
       var settings = $.extend({
         module_name: 'j1.adapter.translator',
-        generated:   '2022-06-29 18:45:20 +0000'
+        generated:   '2022-06-30 13:11:19 +0000'
       }, options);
       // -----------------------------------------------------------------------
       // Global variable settings
