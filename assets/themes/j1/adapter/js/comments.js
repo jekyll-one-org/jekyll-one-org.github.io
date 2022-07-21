3e9dfc7c3e946a20be686b6f31a9d404faff6b35
Juergen Adams <juergen@jekyll.one>
2022-07-21 18:57:06 +0200

    Latest files of version 2022.4.10

--- assets/themes/j1/adapter/js/comments.js
+++ assets/themes/j1/adapter/js/comments.js
@@ -13,7 +13,7 @@
  # J1 Template is licensed under the MIT License.
  # For details, see: https://github.com/jekyll-one-org/j1-template/blob/main/LICENSE.md
  # -----------------------------------------------------------------------------
- #  Adapter generated: 2022-07-21 16:45:36 +0000
+ #  Adapter generated: 2022-07-21 16:55:07 +0000
  # -----------------------------------------------------------------------------
 */
 'use strict';j1.adapter.comments=function(e){var t,n,i;(new Date).toISOString(),document.createElement('script'),document.createElement('script'),document.createElement('script'),'<your-hyvor-site-id>'.includes('your'),e.getCookieNames();return{init:function(){var t=setInterval(function(){'finished'==e.getState()&&((n=log4javascript.getLogger('j1.adapter.comments')).info("\ncomment services: disabled"),clearInterval(t))},25)},messageHandler:function(e,t){var r=JSON.stringify(t,undefined,2);return i="\nreceived message from "+e+': '+r,n.debug(i),'command'===t.type&&'module_initialized'===t.action&&n.info('\n'+t.text),!0},setState:function(e){t.state=e},getState:function(){return t.state}}}(j1,window);
