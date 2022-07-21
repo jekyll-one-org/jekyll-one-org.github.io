7aa39845810cef4ac978cb1d0b37be91839c2b2f
Juergen Adams <juergen@jekyll.one>
2022-07-21 18:29:48 +0200

    Latest files of version 2022.4.10

--- assets/themes/j1/adapter/js/cookieConsent.js
+++ assets/themes/j1/adapter/js/cookieConsent.js
@@ -12,7 +12,7 @@
  #  J1 Template is licensed under MIT License.
  #  See: https://github.com/jekyll-one/J1 Template/blob/master/LICENSE
  # -----------------------------------------------------------------------------
- #  Adapter generated: 2022-07-21 16:11:13 +0000
+ #  Adapter generated: 2022-07-21 16:28:03 +0000
  # -----------------------------------------------------------------------------
 */
 // -----------------------------------------------------------------------------
@@ -64,7 +64,7 @@ j1.adapter.cookieConsent = (function (j1, window) {
       // -----------------------------------------------------------------------
       var settings = $.extend({
         module_name: 'j1.adapter.cookieConsent',
-        generated:   '2022-07-21 16:11:13 +0000'
+        generated:   '2022-07-21 16:28:03 +0000'
       }, options);
       // -----------------------------------------------------------------------
       // Global variable settings
