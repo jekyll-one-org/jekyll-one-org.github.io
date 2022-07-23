6e4b26e8ab8517a243b07a5c391a5fb4a245693c
Juergen Adams <juergen@jekyll.one>
2022-07-23 11:51:53 +0200

    Latest files of version 2022.4.10

--- assets/themes/j1/adapter/js/bmd.js
+++ assets/themes/j1/adapter/js/bmd.js
@@ -13,9 +13,9 @@
  # J1 Template is licensed under the MIT License.
  # For details, see: https://github.com/jekyll-one-org/j1-template/blob/main/LICENSE.md
  # -----------------------------------------------------------------------------
- #  Adapter generated: 2022-07-21 19:31:46 +0000
+ #  Adapter generated: 2022-07-23 09:38:12 +0000
  # -----------------------------------------------------------------------------
 */
-'use strict';j1.adapter.bmd=function(e){var t,n,i;return{init:function(i){$.extend({module_name:'j1.adapter.bmd',generated:'2022-07-21 19:31:46 +0000'},i);t=e.adapter.bmd,n=log4javascript.getLogger('j1.adapter.bmd');var a=setInterval(function(){if('finished'==e.getState()){t.setState('started'),n.debug("\nstate: "+t.getState()),n.info("\nmodule is being initialized");var i="\nBMD is being initialized";n.info(i),$('body').bmd(),t.setState('finished'),n.debug("\nstate: "+t.getState()),n.info("\ninitializing module finished"),clearInterval(a)}},25)},messageHandler:function(e,t){var a=JSON.stringify(t,undefined,2);return i="\nreceived message from "+e+': '+a,n.debug(i),'command'===t.type&&'module_initialized'===t.action&&n.info('\n'+t.text),!0},setState:function(e){t.state=e},getState:function(){return t.state}}}(j1,window);
+'use strict';j1.adapter.bmd=function(e){var t,n,i;return{init:function(i){$.extend({module_name:'j1.adapter.bmd',generated:'2022-07-23 09:38:12 +0000'},i);t=e.adapter.bmd,n=log4javascript.getLogger('j1.adapter.bmd');var a=setInterval(function(){if('finished'==e.getState()){t.setState('started'),n.debug("\nstate: "+t.getState()),n.info("\nmodule is being initialized");var i="\nBMD is being initialized";n.info(i),$('body').bmd(),t.setState('finished'),n.debug("\nstate: "+t.getState()),n.info("\ninitializing module finished"),clearInterval(a)}},25)},messageHandler:function(e,t){var a=JSON.stringify(t,undefined,2);return i="\nreceived message from "+e+': '+a,n.debug(i),'command'===t.type&&'module_initialized'===t.action&&n.info('\n'+t.text),!0},setState:function(e){t.state=e},getState:function(){return t.state}}}(j1,window);
 
 
--- assets/themes/j1/adapter/js/cookieConsent.js
+++ assets/themes/j1/adapter/js/cookieConsent.js
@@ -12,7 +12,7 @@
  #  J1 Template is licensed under MIT License.
  #  See: https://github.com/jekyll-one/J1 Template/blob/master/LICENSE
  # -----------------------------------------------------------------------------
- #  Adapter generated: 2022-07-21 19:31:46 +0000
+ #  Adapter generated: 2022-07-23 09:38:12 +0000
  # -----------------------------------------------------------------------------
 */
 // -----------------------------------------------------------------------------
@@ -64,7 +64,7 @@ j1.adapter.cookieConsent = (function (j1, window) {
       // -----------------------------------------------------------------------
       var settings = $.extend({
         module_name: 'j1.adapter.cookieConsent',
-        generated:   '2022-07-21 19:31:46 +0000'
+        generated:   '2022-07-23 09:38:12 +0000'
       }, options);
       // -----------------------------------------------------------------------
       // Global variable settings
