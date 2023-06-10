99ac20ad64c3076a73db284e3720773360998e55
Juergen Adams <juergen@jekyll.one>
2023-06-10 02:38:02 +0200

    bla!

--- assets/themes/j1/adapter/js/j1.js
+++ assets/themes/j1/adapter/js/j1.js
@@ -13,7 +13,7 @@
  # J1 Theme is licensed under the MIT License.
  # For details, see: https://github.com/jekyll-one-org/j1-template/blob/main/LICENSE.md
  # -----------------------------------------------------------------------------
- # Adapter generated: 2023-06-10 02:03:01 +0200
+ # Adapter generated: 2023-06-10 02:34:48 +0200
  # -----------------------------------------------------------------------------
 */
 // -----------------------------------------------------------------------------
@@ -176,7 +176,7 @@ var j1 = (function (options) {
       // -----------------------------------------------------------------------
       var settings = $.extend({
         module_name: 'j1',
-        generated:   '2023-06-10 02:03:01 +0200'
+        generated:   '2023-06-10 02:34:48 +0200'
       }, options);
       // create settings object from frontmatter options
       var frontmatterOptions  = options != null ? $.extend({}, options) : {};
