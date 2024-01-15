943d0dc9bea654ca1f3c129b9d614407d6817159
Juergen Adams <juergen@jekyll.one>
2024-01-16 00:05:08 +0100

    Latest files of version 2024.1.0

--- assets/themes/j1/adapter/js/chatbot.js
+++ assets/themes/j1/adapter/js/chatbot.js
@@ -13,7 +13,7 @@
  # J1 Template is licensed under the MIT License.
  # For details, see: https://github.com/jekyll-one-org/j1-template/blob/main/LICENSE.md
  # -----------------------------------------------------------------------------
- #  Adapter generated: 2024-01-15 03:15:22 +0100
+ #  Adapter generated: 2024-01-15 22:18:27 +0100
  # -----------------------------------------------------------------------------
 */
 'use strict';j1.adapter.chatbot=function(t,e){var n,a,i;new URL(e.location.href).hostname,document.createElement('script'),t.getCookieNames(),(new Date).toISOString();return{init:function(){var e=setInterval(function(){var n='block'==$('#no_flicker').css("display");t.adapter.attic.getState();'finished'===t.getState()&&n&&((a=log4javascript.getLogger('j1.adapter.chatbot')).info("\nchatbot: disabled"),clearInterval(e))},10)},messageHandler:function(t,e){var n=JSON.stringify(e,undefined,2);return i="\nreceived message from "+t+': '+n,a.debug(i),'command'===e.type&&'module_initialized'===e.action&&a.info('\n'+e.text),!0},setState:function(t){n.state=t},getState:function(){return n.state}}}(j1,window);
